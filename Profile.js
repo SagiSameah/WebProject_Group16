5bca6ed3bfb7a46f36dd821e0d0ee682414b01b6
Sagi Sameah Academic <sagisa@post.bgu.ac.il>
2024-07-05 18:47:44 +0300

    פרופיל בעידכון ללא פרטים אישיים בפועל. צריך לסדר את כפתור עידכון פרטים אישיים להיות מתחת לתמונה

--- Profile.js
+++ Profile.js
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const genderSelect = document.getElementById('gender');
     const passwordInput = document.getElementById('password');
 
-    const genres = ["פנטזיה", "מתח", "רומנטיקה", "מדע בדיוני", "ביוגרפיה"];
-    const authors = ["ש\"י עגנון", "דוד גרוסמן", "צרויה שלו", "עמוס עוז","שגיא שמח","דניאל נוס" ,"אלונה קמחי"];
+    const genres = ["פרוזה","פילוסופיה","פנטזיה", "מתח", "רומנטיקה", "מדע בדיוני", "ביוגרפיה"];
+    const authors = ["ש\"י עגנון", "דוד גרוסמן", "צרויה שלו", "עמוס עוז", "אלונה קמחי","דניאל נוס","שגיא שמח"];
     const selectedGenres = new Set();
     const selectedAuthors = new Set();
 
